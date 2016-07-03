dc66396f8f10a3bcdcb7078965ecf465f32ba52e
Karl the Pagan <karlthepagan@gmail.com>
2016-07-03 15:44:22 -0700

    start of simulation framework, first pass analysis

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,6 +1,8 @@
 import groovy.transform.CompileStatic
 
+import java.nio.FloatBuffer
 import java.nio.IntBuffer
+import java.security.SecureRandom
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -17,8 +19,8 @@ class Main {
     public static void main(String[] argv) {
         Map<String,Map<String,Stats>> playerStats = [:].withDefault {[:].withDefault {new Stats()}}
 
-        InputStream is = Main.class.getResourceAsStream('WoWCombatLog.txt')
-//        FileInputStream is = new FileInputStream('E:\\Battle.net\\World of Warcraft Beta\\Logs\\WoWCombatLog-bak.txt')
+//        InputStream is = Main.class.getResourceAsStream('WoWCombatLog.txt')
+        FileInputStream is = new FileInputStream('E:\\Battle.net\\World of Warcraft Beta\\Logs\\WoWCombatLog-bak.txt')
         BufferedInputStream bis = new BufferedInputStream(is)
         bis.readLines().each {
             String tail = it
@@ -63,15 +65,31 @@ class Main {
                     stats.critgap.put(hpMax - hp)
                 }
             } else {
-                stats.hits.put(total)
+                stats.healHit.put(total)
                 if(over > 0) {
-                    stats.hitgap.put(0)
+                    stats.healHitGap.put(0)
                 } else {
-                    stats.hitgap.put(hpMax - hp)
+                    stats.healHitGap.put(hpMax - hp)
                 }
             }
 
             stats.over.put(over)
+        }
+
+        SecureRandom seeds = new SecureRandom()
+        seeds.setSeed(0)
+
+        Map<String,Map<String,List<Simulation>>> simulations = [:].withDefault {[:].withDefault {[]}}
+
+        playerStats.each { name, spells ->
+            println "$name =============="
+
+            spells.each { spellName, Stats stats ->
+
+                SecureRandom rand = new SecureRandom()
+                simulations.get(name).get(spellName).add(
+                        Simulation.calculate(rand, stats, seeds.nextLong()))
+            }
         }
 
         println playerStats
@@ -80,10 +98,14 @@ class Main {
 
 @CompileStatic
 class Stats {
-    IntBuffer hits = IntBuffer.allocate(500)
-    IntBuffer hitgap = IntBuffer.allocate(500)
+    IntBuffer healHitSrc = IntBuffer.allocate(500)
+    IntBuffer healHit = IntBuffer.allocate(500)
+    IntBuffer healHitGap = IntBuffer.allocate(500)
+    FloatBuffer hittime = FloatBuffer.allocate(500)
+    IntBuffer critSource = IntBuffer.allocate(500)
     IntBuffer crits = IntBuffer.allocate(500)
     IntBuffer critgap = IntBuffer.allocate(500)
+    FloatBuffer crittime = FloatBuffer.allocate(500)
     IntBuffer over = IntBuffer.allocate(500)
 
     public List<Integer> asList(IntBuffer buff) {
@@ -101,25 +123,67 @@ class Stats {
     }
 
     public String toString() {
-        def h = asList hits
+        def h = asList healHit
         def o = asList over
         def c = asList crits
-        def hg = asList hitgap
+        def hg = asList healHitGap
 
         double overheal = 100.0 * (long)o.sum(0l) / ((h.sum(0l) as long) + (long)c.sum(0l))
 
-        double critRate = critRate()
-
-        double naiveCritLoss = 0;
-        if(critRate > 0) {
-
-        }
-
-        return """critRate=${critRate}%
+        return """critRate=${critRate()}%
             hit=[${h.min()}-${ave(h)}-${h.max()}]
-            hitgap=[${hg.min()}-${ave(hg)}-${hg.max()}]
+            healHitGap=[${hg.min()}-${ave(hg)}-${hg.max()}]
             crit=[${c.min()}-${ave(c)}-${c.max()}]
             over=$overheal%
             """.replaceAll(/\n\s+/,' ');
+    }
+}
+
+@CompileStatic
+class Simulation {
+    Stats stats;
+    long seed;
+
+    double confidence;
+
+    double addedCritOverhealingRate;
+    double addedCritBenefit;
+    // TODO need per-effect cast time to measure haste benefit
+    double addedHasteBenefit;
+    // TODO need cause-effect spell damage to measure mastery benefit
+    double addedMasteryBenefit;
+    double addedVersaOverhealingRate;
+    double addedVersaBenefit;
+
+    public static Simulation calculate(Random rand, Stats stats, long seed) {
+        Simulation result = new Simulation()
+
+        if(stats.critRate() <= 0) {
+            // this spell does not crit (TODO provide override)
+            result.addedCritOverhealingRate = result.addedCritBenefit = 0.0
+            result.confidence = 1.0
+        } else {
+            // calculate the percent of existing hits which become crits per 1% crit rate gained
+            double converted = stats.over.position() * 0.01
+
+            // threshold to show hypothesis of crit overheal DR
+            int threshold = 0
+            double overhealingSum = 0
+            double gapSum = 0
+
+            result.confidence = converted / 8
+
+            for(int i = stats.healHitGap.position(); i >= 0; i--) {
+                if(stats.healHitGap.get(i) < stats.healHit.get(i)) {
+                    threshold++;
+                    overhealingSum += stats.healHit.get(i) - stats.healHitGap.get(i)
+                }
+                gapSum += stats.healHitGap.get(i)
+            }
+
+            println "$threshold/${stats.healHitGap.position()} possible, ave at 100% ${overhealingSum / stats.healHitGap.position()} / ${gapSum / stats.healHitGap.position()}, converted at 1% $converted"
+        }
+
+        return result;
     }
 }
